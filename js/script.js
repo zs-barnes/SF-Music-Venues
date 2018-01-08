8f0500848cb0fa66d0aa6d744d9994d28f6b6819
Zachary Barnes <zsbarnes@knox.edu>
2018-01-08 09:34:07 -0600

    Fixed semicolons

--- js/script.js
+++ js/script.js
@@ -475,9 +475,9 @@ function viewModel() {
 		map = new google.maps.Map(document.getElementById('map'), options);
 		// resize map based on its center 
 		google.maps.event.addDomListener(window, 'resize', function () {
-			var center = map.getCenter()
-			google.maps.event.trigger(map, "resize")
-			map.setCenter(center)
+			var center = map.getCenter();
+			google.maps.event.trigger(map, "resize");
+			map.setCenter(center);
 		});
 
 
